7daed9e9eebcc3ff1a6947f75bf6a380ba497907
ledinhtri <ledinhtri.cse.hcmut@gmail.com>
2019-04-09 14:26:14 +0700

    new modules

---
+++ DAT/templates/__static__/js/style/color.js
@@ -0,0 +1,16 @@
+
+const Color = {
+	GREEN : "#58D68D",
+	BLUE : "#5DADE2",
+	YELLOW : "#F4D03F",
+	RED : "#EC7063",
+	GRAY: "#B2BABB",
+	WHITE: "#FDFEFE",
+
+	Opacity_GREEN: "rgba(0,255,0,0.2)",
+	
+	Transparent: "transparent",
+
+}
+
+export {Color}
