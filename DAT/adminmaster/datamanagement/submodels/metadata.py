41673c88f5c5bb849974bbef2d4861c4a2995dba
ledinhtri <ledinhtri.cse.hcmut@gmail.com>
2019-04-10 16:09:37 +0700

    add field bad meta data

--- DAT/adminmaster/datamanagement/submodels/metadata.py
+++ DAT/adminmaster/datamanagement/submodels/metadata.py
@@ -21,6 +21,8 @@ class MetaDataModel(models.Model):
 
    is_onworking = models.BooleanField(default=False, verbose_name="File image is on busy or not")
 
+   is_badmeta = models.BooleanField(default=False, verbose_name="File image is bad or not")
+
    onviewing_user = models.CharField(max_length=1000, default='',blank=True, null=True ,verbose_name="User Viewing")
 
    annotated_by_user = models.ManyToManyField('auth.User', blank=True, related_name="annotated_by_user")
