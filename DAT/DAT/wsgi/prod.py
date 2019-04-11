f28ebfb590022c1b116cad8ec00ae2635eeb486e
Le Dinh Tri <ledinhtri.cse.hcmut@gmail.com>
2019-04-11 14:16:22 +0700

    add more url and path folder

--- DAT/DAT/wsgi/prod.py
+++ DAT/DAT/wsgi/prod.py
@@ -17,4 +17,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DAT.settings.prod')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=settings.STATIC_ROOT, prefix=settings.STATIC_URL)
-
+application.add_files(settings.STORAGE_ROOT, prefix=settings.STORAGE_URL)
+application.add_files(settings.OUTPUT_ROOT, prefix=settings.OUTPUT_URL)
