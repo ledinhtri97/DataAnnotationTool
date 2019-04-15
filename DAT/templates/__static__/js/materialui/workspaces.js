d73a94ba48f44fe1ee5072b82ce861421267a955
ledinhtri <ledinhtri.cse.hcmut@gmail.com>
2019-04-15 23:16:09 +0700

    workspaces UI add data

--- DAT/templates/__static__/js/materialui/workspaces.js
+++ DAT/templates/__static__/js/materialui/workspaces.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { Link } from 'react-router-dom';
-import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import BarChart from '@material-ui/icons/BarChart';
+import OpenInNew from '@material-ui/icons/OpenInNew';
 
 const styles = theme => ({
 	main:{
 		paddingTop: "10px",
 	},
-	icon: {
+	button:{
+		margin: theme.spacing.unit,
+		backgroundColor: "#4285f4",
+	},
+	leftIcon: {
+		padding: "0 3px",
 		marginRight: theme.spacing.unit * 2,
 	},
 	heroUnit: {
@@ -31,7 +34,7 @@ const styles = theme => ({
 		padding: `${theme.spacing.unit * 4}px 0 ${theme.spacing.unit * 3}px`,
 	},
 	heroButtons: {
-		marginTop: theme.spacing.unit * 4,
+		marginTop: theme.spacing.unit * 3,
 	},
 	layout: {
 		width: 'auto',
@@ -44,7 +47,7 @@ const styles = theme => ({
 		},
 	},
 	cardGrid: {
-		padding: `${theme.spacing.unit * 6}px 0`,
+		padding: `${theme.spacing.unit * 5}px 0`,
 	},
 	card: {
 		height: '100%',
@@ -86,7 +89,7 @@ class Workspaces extends React.Component {
 
 		const { classes } = this.props;
 		const workspace = this;
-		const workspaces = JSON.parse(document.getElementById("datawps").textContent)['workspaces'];
+		const workspaces = JSON.parse(document.getElementById("home").textContent)['workspaces'];
 		return (
 			<React.Fragment>
 			<main className={classes.main}>
@@ -123,7 +126,7 @@ class Workspaces extends React.Component {
           			<Grid item key={key} sm={6} md={4} lg={3}>
           			<Card className={classes.card}>
           			<CardMedia
-          			className={classes.cardMedia} image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" title="Image title"/>
+          			className={classes.cardMedia} image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" title={wp.name}/>
           			<CardContent className={classes.cardContent}>
           			<Typography gutterBottom variant="h5" component="h2">
           			{wp.name}
@@ -135,11 +138,13 @@ class Workspaces extends React.Component {
           			</Typography>
           			</CardContent>
           			<CardActions>
-          			<Button onClick={function(e){btnClick.handleOverview()}} size="small" color="primary">
+          			<Button onClick={function(e){btnClick.handleOverview()}} color="primary" size="small">
+          			<BarChart className={classes.rightIcon} />
           			OVERVIEW
           			</Button>
-          			<Button onClick={function(e){btnClick.handleStartLabeling()}} size="small" color="primary">
-          			START LABELING
+          			<Button onClick={function(e){btnClick.handleStartLabeling()}} variant="contained" size="small" color="primary" className={classes.button}>
+          			<OpenInNew className={classes.rightIcon} />
+          			LABELING
           			</Button>
           			</CardActions>
           			</Card>
