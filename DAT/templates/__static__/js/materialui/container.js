f1aca7a2314b89336689137cd731adbce40b58f5
ledinhtri <ledinhtri.cse.hcmut@gmail.com>
2019-04-15 23:14:52 +0700

    container UI

--- DAT/templates/__static__/js/materialui/container.js
+++ DAT/templates/__static__/js/materialui/container.js
@@ -1,48 +1,391 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SkipNext from '@material-ui/icons/SkipNext';
+import Beenhere from '@material-ui/icons/Beenhere';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 
-const styles = theme => ({
-	appBarSpacer: theme.mixins.toolbar,
-	content: {
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+import Chip from '@material-ui/core/Chip';
+import classNames from 'classnames';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+
+import generalListItems from './listitem/generalListItems';
+import historyListItems from './listitem/historyListItems';
+import toolListItems from './listitem/toolListItems';
+import labelListItems from './listitem/labelListItems';
+
+import {outWorkSpace} from "../modules/dat-utils"
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import Tooltip from '@material-ui/core/Tooltip';
+import Zoom from '@material-ui/core/Zoom';
+
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
+
+const drawerWidth = 240;
+
+const styles = theme =>({
+	root: {
+		display: 'flex',
+		width: '100%',
+		height: '100%',
+	},
+	root_appbar:{
+		backgroundColor: "#4285F4",
+		minHeight: "45px",
+	},
+	grow: {
 		flexGrow: 1,
-		padding: theme.spacing.unit * 3,
-		height: '100vh',
-		overflow: 'auto',
 	},
-	chartContainer: {
-		marginLeft: -22,
+	menuButton: {
+		marginLeft: 12,
+		marginRight: 36,
 	},
-	tableContainer: {
-		height: 320,
+	chip: {
+		margin: theme.spacing.unit,
+		backgroundColor: "#FFFFFF",
+		color: "#0844a7",
+		fontSize: '1rem',
 	},
-	h5: {
-		marginBottom: theme.spacing.unit * 2,
+
+	menuButtonHidden: {
+		display: 'none',
 	},
+
+	appBar: {
+		zIndex: theme.zIndex.drawer + 1,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+	},
+	appBarShift: {
+		marginLeft: drawerWidth,
+		width: `calc(100% - ${drawerWidth}px)`,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	},
+	toolbarIcon: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+		padding: '0 8px',
+		...theme.mixins.toolbar,
+	},
+	toolbar: {
+    	paddingRight: 24, // keep right padding when drawer closed
+    },
+    
+    drawerPaper: {
+    	position: 'relative',
+    	whiteSpace: 'nowrap',
+    	width: drawerWidth,
+    	transition: theme.transitions.create('width', {
+    		easing: theme.transitions.easing.sharp,
+    		duration: theme.transitions.duration.enteringScreen,
+    	}),
+    },
+    drawerPaperClose: {
+    	overflowX: 'hidden',
+    	transition: theme.transitions.create('width', {
+    		easing: theme.transitions.easing.sharp,
+    		duration: theme.transitions.duration.leavingScreen,
+    	}),
+    	width: theme.spacing.unit * 7,
+    	[theme.breakpoints.up('sm')]: {
+    		width: theme.spacing.unit * 9,
+    	},
+    },
+    mainboard:{
+    	width: '100%',
+    	height: '100%',
+    },
+    span:{
+    	display: 'inline-flex',
+    },
+    heading: {
+    	fontSize: theme.typography.pxToRem(15),
+    	flexBasis: '33.33%',
+    	flexShrink: 0,
+    },
+    tabExpandTitle:{
+    	paddingRight: '0 !important',
+    	paddingLeft: '16px !important',
+    },
+    tabExpandSumary:{
+    	margin: "3px 0 !important",
+    },
+    tabExpandDetail:{
+    	padding: 0,
+    },
+    listItem: {
+    	width: '100%',
+    },
+    padcontroller:{
+    	marginLeft: "50px",
+	},
+	lightTooltip: {
+        backgroundColor: theme.palette.common.white,
+        color: 'rgba(0, 0, 0, 0.87)',
+        boxShadow: theme.shadows[1],
+        fontSize: 11,
+    }
 });
 
-class MainAppContainer extends React.Component {
+// {/*<IconButton className={classes.menuButton} color="inherit" aria-label="Menu">*/}
+
+class MenuAppBar extends React.Component {
+	
 	state = {
-		something: null,
+		anchorEl: null,
+		open: true,
+		expanded: 'panel1',
 	};
 
+	handleExpandInDrawer = panel => (event, expanded) => {
+		this.setState({
+			expanded: expanded ? panel : false,
+		});
+	};
 
+	handleDrawerOpen = () => {
+		if(document.getElementById("home") == null){
+			this.setState({ open: true });
+		}
+	};
+
+	handleDrawerClose = () => {
+		this.setState({ open: false });
+	}
+
+	handleChange = event => {
+		this.setState({ auth: event.target.checked });
+	};
+
+	handleMenu = event => {
+		this.setState({ anchorEl: event.currentTarget });
+	};
+
+	handleClose = () => {
+		this.setState({ anchorEl: null });
+	};
+
+	handleWorkspace = () => {
+		this.setState({ anchorEl: null });
+		var metaid = document.getElementById("meta_id");
+		if(metaid){
+			outWorkSpace(metaid.textContent, "/gvlab-dat/workspace/");
+		}
+		else{
+			window.location.href = "/gvlab-dat/workspace/";
+		}
+	};
+	
+	handleLogout = () => {
+		this.setState({ anchorEl: null });
+		var metaid = document.getElementById("meta_id");
+		if(metaid){
+			outWorkSpace(metaid.textContent, "/gvlab-dat/logout/");
+		}
+		else{
+			window.location.href = "/gvlab-dat/logout/";
+		}
+	}
+
+	render() {
+		const { classes } = this.props;
+		const { anchorEl, expanded } = this.state;
+		const open = Boolean(anchorEl);
+
+		const ON_HOMEPAGE =  document.getElementById("home") != null;
+		const ON_WORKSPACE = document.getElementById("project") != null;
+		const ON_WORKING = document.getElementById("meta_id") != null;
+		
+		return (	
+			<div className={classes.root}>
+			<CssBaseline />
+			<AppBar position={ON_HOMEPAGE ? "static" : "absolute"}
+			className={classNames(classes.root_appbar, !ON_HOMEPAGE && classes.appBar, !ON_HOMEPAGE && this.state.open && classes.appBarShift)}>
+			<Toolbar disableGutters={!this.state.open} className={classes.toolbar}>
+			<IconButton
+			color="inherit"
+			aria-label="Open drawer"
+			onClick={this.handleDrawerOpen}
+			className={classNames(classes.menuButton, !ON_HOMEPAGE && this.state.open && classes.menuButtonHidden,)}>
+			{!ON_HOMEPAGE && (<MenuIcon />)}
+			</IconButton>
+			<Typography variant="h6" color="inherit" className={classes.grow}>Data Annotation Tool - GVLab</Typography>
+			<div>
+			{
+				ON_WORKING && (
+					<React.Fragment>
+					<Tooltip 
+						title="Skip and next" 
+						TransitionComponent={Zoom} 
+						placement="bottom" 
+						classes={{tooltip: classes.lightTooltip}}>
+					<IconButton  aria-haspopup="true"color="inherit">
+					<SkipNext />
+					</IconButton>
+					</Tooltip>
+					<Tooltip 
+						title="Save and continue" 
+						TransitionComponent={Zoom} 
+						placement="bottom" 
+						classes={{tooltip: classes.lightTooltip}}>
+					<IconButton aria-haspopup="true" color="inherit">
+					<Beenhere />
+					<KeyboardArrowRight />
+					</IconButton>
+					</Tooltip>
+					</React.Fragment>)
+			}
+			<Chip label={document.getElementById("username").textContent} className={classNames(classes.chip, ON_WORKING && classes.padcontroller)} />
+			{ON_WORKING && <span id="keyboard" className={classes.span}/>}
+			<Tooltip 
+						title="Account" 
+						TransitionComponent={Zoom} 
+						placement="bottom" 
+						classes={{tooltip: classes.lightTooltip}}>
+			<IconButton 
+				aria-owns={open ? 'menu-appbar' : undefined} 
+				aria-haspopup="true" onClick={this.handleMenu} color="inherit">
+			<AccountCircle />
+			</IconButton>
+			</Tooltip>
+			<Menu
+			id="menu-appbar"
+			anchorEl={anchorEl}
+			anchorOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			transformOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			open={open}
+			onClose={this.handleClose}
+			>
+		{/*<MenuItem onClick={this.handleClose}>Profile</MenuItem>*/}
+		<MenuItem onClick={this.handleWorkspace}>Main Workspace</MenuItem>
+		<MenuItem onClick={this.handleLogout}>Log out</MenuItem>
+		</Menu>
+		</div>
+		</Toolbar>
+		</AppBar>
+
+		{ON_WORKSPACE && (<Drawer
+			variant="permanent"
+			classes={{
+				paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
+			}}
+			open={this.state.open}
+			>
+			<div className={classes.toolbarIcon}>
+			<IconButton onClick={this.handleDrawerClose}>
+			<ChevronLeftIcon />
+			</IconButton>
+			</div>
+			<Divider />
+			<List className={classes.listItem}>{React.createElement(generalListItems)}</List>
+			<Divider />
+			<List className={classes.listItem}>{React.createElement(historyListItems)}</List>
+			</Drawer>
+			)}
+
+		{ON_WORKING && (<Drawer
+			variant="permanent"
+			classes={{
+				paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
+			}}
+			open={this.state.open}
+			>
+			<div className={classes.toolbarIcon}>
+
+			<span id="settings" title="Settings" className={classes.span}/>
+
+			<IconButton onClick={this.handleDrawerClose}>
+			<ChevronLeftIcon />
+			</IconButton>
+			</div>
+
+
+			<ExpansionPanel expanded={expanded === 'panel1'} onChange={this.handleExpandInDrawer('panel1')}>
+			<ExpansionPanelSummary classes={{content: classes.tabExpandSumary}}>
+			<ListItem button className={classes.tabExpandTitle}>
+			<ListItemText primary="Tools"/>
+			</ListItem>
+			</ExpansionPanelSummary>
+			<ExpansionPanelDetails className={classes.tabExpandDetail}>
+			<Divider />
+			<List className={classes.listItem}>{React.createElement(toolListItems)}</List>
+			</ExpansionPanelDetails>
+			</ExpansionPanel>
+
+			<ExpansionPanel expanded={expanded === 'panel2'} onChange={this.handleExpandInDrawer('panel2')}>
+			<ExpansionPanelSummary classes={{content: classes.tabExpandSumary}}>
+			<ListItem button className={classes.tabExpandTitle}>
+			<ListItemText primary="Labels"/>
+			</ListItem>
+			</ExpansionPanelSummary>
+			<ExpansionPanelDetails className={classes.tabExpandDetail}>
+			<Divider />
+			<List className={classes.listItem}>{React.createElement(labelListItems)}</List>
+			</ExpansionPanelDetails>
+			</ExpansionPanel>
+
+
+			<ExpansionPanel expanded={expanded === 'panel3'} onChange={this.handleExpandInDrawer('panel3')}>
+			<ExpansionPanelSummary classes={{content: classes.tabExpandSumary}}>
+			<ListItem button className={classes.tabExpandTitle}>
+			<ListItemText primary="General" />
+			</ListItem>
+			</ExpansionPanelSummary>
+			<ExpansionPanelDetails className={classes.tabExpandDetail}>
+			<Divider />
+			<List className={classes.listItem}>{React.createElement(generalListItems)}</List>
+			</ExpansionPanelDetails>
+			</ExpansionPanel>
+
+
+			</Drawer>
+			)}
+
+
+		{ON_WORKING && <div id="mainboard" className={classes.mainboard}></div>}
+
+		</div>	
+		);
+}
 }
 
-MainAppContainer.propTypes = {
+MenuAppBar.propTypes = {
 	classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(MainAppContainer);
+export default withStyles(styles)(MenuAppBar);
