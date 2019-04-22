de3e15618f6a182c6ae63f09619db04379c25292
ledinhtri <ledinhtri.cse.hcmut@gmail.com>
2019-04-23 00:17:23 +0700

    disable api predict

--- DAT/templates/__static__/js/api/personRequest.js
+++ DAT/templates/__static__/js/api/personRequest.js
@@ -1,30 +1,30 @@
-import {createItemToBoundingBoxes} from '../controller/itemReact';
-import {configureRectangle} from '../drawer/polygon';
+// import {createItemToBoundingBoxes} from '../controller/itemReact';
+// import {configureRectangle} from '../drawer/polygon';
 
-const requestPersonAPI = function(meta_id, canvas){
-	fetch("/gvlab-dat/workspace/persondet/"+meta_id, {metaid: meta_id})
-	.then(response => {
-		if(response.status !== 200){
-			return "Something went wrong"
-		}
-		return response.json();
-	})
-	.then(data => {
-		data['resAPI'].map(function(face, index){
-			var width = canvas.width;
-			var height = canvas.height;
+// const requestPersonAPI = function(meta_id, canvas){
+// 	fetch("/gvlab-dat/workspace/persondet/"+meta_id, {metaid: meta_id})
+// 	.then(response => {
+// 		if(response.status !== 200){
+// 			return "Something went wrong"
+// 		}
+// 		return response.json();
+// 	})
+// 	.then(data => {
+// 		data['resAPI'].map(function(face, index){
+// 			var width = canvas.width;
+// 			var height = canvas.height;
 
-			var bbface = configureRectangle(
-					width*face.xmin, 
-					height*face.ymin,
-					width*(face.xmax-face.xmin),
-					height*(face.ymax-face.ymin),
-					'person');
-			canvas.add(bbface);
-			canvas.renderAll();
+// 			var bbface = configureRectangle(
+// 					width*face.xmin, 
+// 					height*face.ymin,
+// 					width*(face.xmax-face.xmin),
+// 					height*(face.ymax-face.ymin),
+// 					'person');
+// 			canvas.add(bbface);
+// 			canvas.renderAll();
 
-			createItemToBoundingBoxes(canvas, 'person');
-	});
-});}
+// 			createItemToBoundingBoxes(canvas, 'person');
+// 	});
+// });}
 
-export {requestPersonAPI};
+// export {requestPersonAPI};
