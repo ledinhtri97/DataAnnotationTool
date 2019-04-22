cd306cf4129b8b59567d83ccc9e8b39679f13f46
ledinhtri <ledinhtri.cse.hcmut@gmail.com>
2019-04-23 00:15:31 +0700

    admin site add app display

--- DAT/adminmaster/workspacemanagement/admin.py
+++ DAT/adminmaster/workspacemanagement/admin.py
@@ -1,6 +1,78 @@
 from django.contrib import admin
+from django import forms
+import json
+from usermaster.models import UserSettingsModel
 from adminmaster.workspacemanagement.models import WorkSpaceUserModel
-
+from adminmaster.datamanagement.models import DataSetModel
 # Register your models here.
 
-admin.site.register(WorkSpaceUserModel)
+class WorkSpaceUserForm(forms.ModelForm):
+    class Meta:
+      model = WorkSpaceUserModel
+      fields = '__all__'
+
+    def generate_settings(self, dataset, users):
+      
+      print(dataset.id)
+      
+      sett_wdata = UserSettingsModel.objects.filter(dataset=dataset)
+
+      labels = DataSetModel.objects.filter(id=dataset.id).first().labels.all()
+
+      colors_map = {
+        str(lb):lb.color for lb in labels
+      }
+
+      setts = json.dumps({
+        'show_popup': 'false',
+        'auto_hidden': 'false',
+        'auto_predict': 'false',
+        'ask_dialog': 'true',
+        'color_background': 'true',
+        'size_icon': 8,
+        'width_stroke': 3,
+      })
+
+      for u in users.all():
+        if (not sett_wdata.filter(user=u).first()):
+          UserSettingsModel.objects.get_or_create(dataset=dataset, user=u, settings=setts)
+          print(u)
+
+    def save(self, commit=True):
+      instance = super(WorkSpaceUserForm, self).save(commit=False)
+      instance.save()
+      
+      self.generate_settings(
+        self.cleaned_data['dataset'],
+        self.cleaned_data['user']
+      )
+
+      if(commit):
+         instance.save()
+
+      return instance
+
+class WorkSpaceUserAdmin(admin.ModelAdmin):
+   #class Meta will not accept this form custom > find out why
+   form = WorkSpaceUserForm
+
+   fieldsets = [
+      (None,
+            {
+               'fields': ['nameworkspace']
+            }
+      ),
+      ('DATASET Field',
+            {
+                'fields': ['dataset']
+            }
+      ),
+      ('USERS Field',
+            {
+                'fields': ['user']
+            }
+      ),
+   ]
+
+admin.site.register(WorkSpaceUserModel, WorkSpaceUserAdmin)
+admin.site.register(UserSettingsModel)
