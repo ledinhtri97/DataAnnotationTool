dc3316bf491875d6fe446e2edbc220bc05a324e8
ledinhtri <ledinhtri.cse.hcmut@gmail.com>
2019-04-23 00:09:10 +0700

    label data model add color field

--- DAT/adminmaster/datamanagement/submodels/labeldata.py
+++ DAT/adminmaster/datamanagement/submodels/labeldata.py
@@ -1,85 +1,83 @@
 from django.db import models
 from simple_history.models import HistoricalRecords
-
+from django.core.validators import FileExtensionValidator
+from django.conf import settings
+from colorfield.fields import ColorField
+import os
 
 class LabelDataModel(models.Model):
 
      RECT = 'rect'
      QUAD = 'quad'
      POLY = 'poly'
-
      TYPE_LABEL_CHOICES = (
         (RECT, 'Rectangle'),
         (QUAD, 'Quadrilateral'),
         (POLY, 'Polygon'),
      )
-
-     FACE = 'face'
-     PERSON = 'person'
-     AEROPLANE = 'aeroplane'
-     BICYCLE = 'bicycle'
-     BIRD = 'bird'
-     BOAT = 'boat'
-     BOTTLE = 'bottle'
-     BUS = 'bus'
-     CAR = 'car'
-     CAT = 'cat'
-     CHAIR = 'chair'
-     COW = 'cow'
-     DININGTABLE = 'diningtable'
-     DOG = 'dog'
-     HORSE = 'horse'
-     MOTORBIKE = 'motorbike'
-     POTTEDPLANT = 'pottedplant'
-     SHEEP = 'sheep'
-     SOFA = 'sofa'
-     TRAIN = 'train'
-     TVMONITOR = 'tvmonitor'
-     TEXT = 'text'
-
-
-     LABEL_MAP_CHOICES = (
-          (FACE, 'face'),
-          (PERSON, 'person'),
-          (AEROPLANE, 'aeroplane'),
-          (BICYCLE, 'bicycle'),
-          (BIRD, 'bird'),
-          (BOAT, 'boat'),
-          (BOTTLE, 'bottle'),
-          (BUS, 'bus'),
-          (CAR, 'car'),
-          (CAT, 'cat'),
-          (CHAIR, 'chair'),
-          (COW, 'cow'),
-          (DININGTABLE, 'diningtable'),
-          (DOG, 'dog'),
-          (HORSE, 'horse'),
-          (MOTORBIKE, 'motorbike'),
-          (POTTEDPLANT, 'pottedplant'),
-          (SHEEP, 'sheep'),
-          (SOFA, 'sofa'),
-          (TRAIN, 'train'),
-          (TVMONITOR, 'tvmonitor'),
-          (TEXT, 'text'),
-     )
-
      id = models.AutoField(primary_key=True)
 
-     tag_label = models.CharField(
-          max_length=10,
-          choices=LABEL_MAP_CHOICES,
-          default=FACE,
-     )
+     tag_label = models.CharField(max_length=100, default='label', verbose_name="Name label")
 
-     type_label = models.CharField(
+     type_label = models.CharField (
           max_length=20,
           choices=TYPE_LABEL_CHOICES,
           default=RECT,
      )
 
+     color = ColorField(default='#58D68D')
+     
      description = models.CharField(max_length=1000, blank=True, null=True)
 
      history = HistoricalRecords()
 
      def __str__(self):
           return self.tag_label+"-"+self.type_label
+
+     # FACE = 'face'
+     # PERSON = 'person'
+     # AEROPLANE = 'aeroplane'
+     # BICYCLE = 'bicycle'
+     # BIRD = 'bird'
+     # BOAT = 'boat'
+     # BOTTLE = 'bottle'
+     # BUS = 'bus'
+     # CAR = 'car'
+     # CAT = 'cat'
+     # CHAIR = 'chair'
+     # COW = 'cow'
+     # DININGTABLE = 'diningtable'
+     # DOG = 'dog'
+     # HORSE = 'horse'
+     # MOTORBIKE = 'motorbike'
+     # POTTEDPLANT = 'pottedplant'
+     # SHEEP = 'sheep'
+     # SOFA = 'sofa'
+     # TRAIN = 'train'
+     # TVMONITOR = 'tvmonitor'
+     # TEXT = 'text'
+     
+     # LABEL_MAP_CHOICES = (
+     #      (FACE, 'face'),
+     #      (PERSON, 'person'),
+     #      (AEROPLANE, 'aeroplane'),
+     #      (BICYCLE, 'bicycle'),
+     #      (BIRD, 'bird'),
+     #      (BOAT, 'boat'),
+     #      (BOTTLE, 'bottle'),
+     #      (BUS, 'bus'),
+     #      (CAR, 'car'),
+     #      (CAT, 'cat'),
+     #      (CHAIR, 'chair'),
+     #      (COW, 'cow'),
+     #      (DININGTABLE, 'diningtable'),
+     #      (DOG, 'dog'),
+     #      (HORSE, 'horse'),
+     #      (MOTORBIKE, 'motorbike'),
+     #      (POTTEDPLANT, 'pottedplant'),
+     #      (SHEEP, 'sheep'),
+     #      (SOFA, 'sofa'),
+     #      (TRAIN, 'train'),
+     #      (TVMONITOR, 'tvmonitor'),
+     #      (TEXT, 'text'),
+     # )
