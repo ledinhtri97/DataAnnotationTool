490a7106230b01bace4e1341743a065644f6f37c
ledinhtri <ledinhtri.cse.hcmut@gmail.com>
2019-04-23 00:19:51 +0700

    popup control remove E D H

--- DAT/templates/__static__/js/controller/popup.js
+++ DAT/templates/__static__/js/controller/popup.js
@@ -1,76 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {ElementITEM} from "./itemReact";
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
+import {quickSettings, drawStatus} from '../main_module'
+// import {quickSettings} from '../ui_module';
 
 class PopupControllers{
 
 	constructor(__canvas__){
 		const PC = this;
 		PC.canvas = __canvas__;
-		PC.ispopup = false;
-
-		var btn_delete = document.getElementById("btn_delete");
-		var btn_hidden = document.getElementById("btn_hidden");
-		var btn_edit = document.getElementById("btn_edit");
-
-		PC.btn_hidden_event = function(e){
-			document.getElementById("groupcontrol").style["display"] = "none";
-			var iitem = PC.canvas.getObjects().indexOf(PC.objshape)
-			var current_element = document.getElementById("itembb_"+iitem);
-			var icheckbox = current_element.firstElementChild.children[1].firstElementChild;
-			icheckbox.checked = !icheckbox.checked;
-
-			var wrapper = shallow(<ElementITEM canvas={PC.canvas} objshape={PC.objshape}/>); 
-			wrapper.find(
-				'input[type="checkbox"]'
-				).at(0).simulate('change',{ target: { checked: icheckbox.checked } });
-		}
-
-		PC.btn_edit_event = function(e){
-			document.getElementById("groupcontrol").style["display"] = "none";
-			var iitem = PC.canvas.getObjects().indexOf(PC.objshape)
-			var current_element = document.getElementById("itembb_"+iitem);
-			var icheckbox = current_element.firstElementChild.children[2].firstElementChild;
-
-			icheckbox.checked = !icheckbox.checked;
-			var wrapper = shallow(<ElementITEM canvas={PC.canvas} objshape={PC.objshape}/>);
-			wrapper.find(
-				'input[type="checkbox"]'
-				).at(1).simulate('change',{ target: { checked: icheckbox.checked } });
-		}
-
-		PC.btn_delete_event = function(e){
-			var wrapper = shallow(<ElementITEM canvas={PC.canvas} objshape={PC.objshape} />);
-			wrapper.find('input[type="button"]').simulate('click');
-			document.getElementById("groupcontrol").style["display"] = "none";
-		}
-
-		btn_delete.addEventListener('click', PC.btn_delete_event);
-		btn_hidden.addEventListener('click', PC.btn_hidden_event);
-		btn_edit.addEventListener('click', PC.btn_edit_event);
-	}
-
-	setPopup = function(value){
-		this.ispopup = value;
 	}
 
 	popup = function(__obj__){
-		this.objshape = __obj__;
-		var groupcontrol = document.getElementById("groupcontrol");
-		if(this.ispopup && groupcontrol) {
+		this.objshape = __obj__.name_id != 'icon' ? __obj__ : __obj__.object;
+
+		var group_control = document.getElementById("group_control");
+		var canPopup = !drawStatus.getIsDrawing() || (drawStatus.getIsWaiting() && drawStatus.getIsDrawing())
+		if(quickSettings.getAtt('show_popup') && group_control && canPopup) {
 			
 			var cvcontainer = document.getElementById("cvcontainer");
 			var cv_element = document.getElementById("canvas");
-			document.getElementById("labelpopup").textContent = this.objshape.name;
-			document.getElementById("accuracypopup").textContent = this.objshape.accuracy;
+			document.getElementById("label_popup").textContent = this.objshape.name;
+			document.getElementById("accuracy_popup").textContent = this.objshape.accuracy;
 			var left = (cvcontainer.clientWidth - cv_element.clientWidth) / 2;
 			var top = (cvcontainer.clientHeight - cv_element.clientHeight) / 2;
-			groupcontrol.style["left"] = (left + this.objshape.oCoords.tl.x) +"px";
-			groupcontrol.style["top"] = (top + this.objshape.oCoords.tl.y - 40)+"px";
-			groupcontrol.style["display"] = "";
+			group_control.style["left"] = (left + this.objshape.oCoords.bl.x) +"px";
+			group_control.style["top"] = (top + this.objshape.oCoords.bl.y)+"px";
+			group_control.style["display"] = "";
 		}
 	}
 }
