2bcb0ad81fc97098342e285ef1729015aec99e09
ledinhtri <ledinhtri.cse.hcmut@gmail.com>
2019-04-25 00:04:53 +0700

    remove models UserSettings locate in adminmaster

--- DAT/usermaster/models.py
+++ DAT/usermaster/models.py
@@ -1,18 +1,2 @@
 from django.db import models
-from adminmaster.datamanagement.models import DataSetModel
-from adminmaster.workspacemanagement.models import WorkSpaceUserModel
-
-# Create your models here.
-
-class UserSettingsModel(models.Model):
-  
-  user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-
-  dataset = models.ForeignKey(DataSetModel, on_delete=models.CASCADE)
-
-  settings = models.TextField(default='', blank=True, null=True)
-  
-  def __str__(self):
-      return self.user.username + " | " + str(self.dataset)
-  
 
