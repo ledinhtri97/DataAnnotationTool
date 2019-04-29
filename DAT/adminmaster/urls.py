e479e2fdd0fab6ab6fc0ae01f8b12ed83278d51c
Le Dinh Tri <ledinhtri.cse.hcmut@gmail.com>
2019-04-29 23:17:18 +0700

    add link contribute management url

--- DAT/adminmaster/urls.py
+++ DAT/adminmaster/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.contrib import admin
+from adminmaster.contributemanagement.views import ContributeView, contribute_view, contribute_accept, contribute_save
 
 urlpatterns = [path('', TemplateView.as_view(
     template_name='adminmaster/index.html'), name='datadmin'),
     path('dataman/', include('adminmaster.datamanagement.urls')),
     path('admin/', admin.site.urls, name='admin'),
+    path('contributes/<int:id_contribute>/', ContributeView.as_view(), name='contributes'),
+    path('contribute_view/<int:id_file>/',
+         contribute_view, name='contribute_view'),
+    path('contribute_accept/<int:id_file>/',
+         contribute_accept, name='contribute_accept'),
+    path('contribute_save/<int:id_file>/',
+         contribute_save, name='contribute_save'),
 ]
