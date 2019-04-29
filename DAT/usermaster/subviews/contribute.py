fde7d8f4cc1c571193aac52054288ee99fde508e
Le Dinh Tri <ledinhtri.cse.hcmut@gmail.com>
2019-04-29 10:56:01 +0700

    response json data user contribute table

--- DAT/usermaster/subviews/contribute.py
+++ DAT/usermaster/subviews/contribute.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from rest_framework.response import Response
 from adminmaster.contributemanagement.models import ContributeModel
 from usermaster.serializers import ContributeSerializer
-
+from adminmaster.datamanagement.submodels.inputdata import InputDataModel
 import json
 from django.http import JsonResponse
 
@@ -13,10 +13,24 @@ class ContributeView(generics.RetrieveAPIView):
   lookup_field = 'id'
   template_name = 'usermaster/contribute.html'
   
-  #@Overwrite
   def get_queryset(self):
     user = self.request.user
     return ContributeModel.objects.filter(available=1).all()
+
+  def get_queryset_user_contrib(self):
+    user_contrib = []
+    inputs = InputDataModel.objects.filter(owner=self.request.user)
+    for ip in inputs.all():
+      contrib_temp = ContributeModel.objects.filter(user=self.request.user, input=ip).first() 
+      user_contrib.append({
+      'contribute_name': contrib_temp.name,
+      'activate': contrib_temp.available,
+      'file_name': ip.get_zipname(),
+      'date_upload': ip.history.first().history_date,
+      'validate': ip.useful,
+    })
+    #print(user_contrib)
+    return user_contrib
 
   #@Overwrite
   def retrieve(self, request, *args, **kwargs):
@@ -29,7 +43,8 @@ class ContributeView(generics.RetrieveAPIView):
 
     data = json.loads(data.content.decode('utf8').replace("'", '"'))
 
+    user_contrib = self.get_queryset_user_contrib()
 
     # print(data)
     
-    return Response(data={'data': data, 'user_contrib': 'user'})
+    return Response(data={'data': data, 'user_contrib': user_contrib})
