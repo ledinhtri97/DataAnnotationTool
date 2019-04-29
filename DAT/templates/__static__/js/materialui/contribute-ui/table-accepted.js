80dcaf26d589e4c77609c0315e880b0d04106f82
Le Dinh Tri <ledinhtri.cse.hcmut@gmail.com>
2019-04-29 23:23:08 +0700

    template build ui contribute management

---
+++ DAT/templates/__static__/js/materialui/contribute-ui/table-accepted.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import AttachFile from '@material-ui/icons/AttachFile';
+import Cookie from 'js-cookie';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 600,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+  form: {
+    display: 'flex',
+  },
+  table_title: {
+    fontSize: '0.967rem',
+  },
+  table_content: {
+    fontSize: '0.925rem',
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
+  },
+});
+
+const CSRFToken = () => {
+  return (
+    <input type="hidden" name="csrfmiddlewaretoken" value={Cookie.get("csrftoken")} />
+    );
+};
+
+// var currentUpload = null;
+
+class RequestTable extends React.Component {
+
+  state = {
+    currentUpload: null,
+  };
+
+  changeTitle = (id, event) => {
+
+    var status = document.getElementById("contribute_status")
+    status.textContent = "Files Upload: "+event.target.files[0].name;
+    status.style['color'] = "#8C0C4B";
+
+    this.setState({ currentUpload: id });
+  }
+
+  render() {
+
+    const { classes } = this.props;
+
+    const contribute_accepted = JSON.parse(
+      document.getElementById("contribute_accepted").textContent
+      )['contribute_accepted'];
+    contribute_accepted.pop();
+    const table = this;
+
+    return (
+      <Paper className={classes.root}>
+      <Table className={classes.table}>
+      <TableHead>
+      <TableRow>
+      <TableCell className={classes.table_title}>User Name</TableCell>
+      <TableCell align="right" className={classes.table_title}>File name</TableCell>
+      <TableCell align="right" className={classes.table_title}>Date Upload</TableCell>
+      <TableCell align="right" className={classes.table_title}>Date Accept</TableCell>
+      <TableCell align="center" className={classes.table_title}>Edit</TableCell>
+      </TableRow>
+      </TableHead>
+      <TableBody>
+      {
+        contribute_accepted.map(function(ct, key){
+          return (
+            <TableRow key={key}>
+            <TableCell component="th" scope="row" className={classes.table_content}>
+            {ct.user_name}
+            </TableCell>
+            <TableCell align="right" className={classes.table_content}>{ct.file_name}</TableCell>
+            <TableCell align="right" className={classes.table_content}>{ct.date_upload}</TableCell>
+            <TableCell align="right" className={classes.table_content}>{ct.date_accept}</TableCell>
+            <TableCell align="center" className={classes.table_content}>
+            <Button variant="contained" className={classes.button}>
+            Edit
+            </Button>
+            </TableCell>
+            </TableRow>
+            );
+        })
+      }
+
+      </TableBody>
+      </Table>
+      </Paper>
+      );
+  }
+}
+
+RequestTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RequestTable);
---
+++ DAT/templates/__static__/js/materialui/contribute-ui/table-request.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import AttachFile from '@material-ui/icons/AttachFile';
+import Cookie from 'js-cookie';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 600,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+  form: {
+    display: 'flex',
+  },
+  table_title: {
+    fontSize: '0.967rem',
+  },
+  table_content: {
+    fontSize: '0.925rem',
+  },
+});
+
+const CSRFToken = () => {
+  return (
+    <input type="hidden" name="csrfmiddlewaretoken" value={Cookie.get("csrftoken")} />
+    );
+};
+
+// var currentUpload = null;
+
+class RequestTable extends React.Component {
+
+  state = {
+    currentUpload: null,
+  };
+
+  changeTitle = (id, event) => {
+
+    var status = document.getElementById("contribute_status")
+    status.textContent = "Files Upload: "+event.target.files[0].name;
+    status.style['color'] = "#8C0C4B";
+
+    this.setState({ currentUpload: id });
+  }
+
+  render() {
+
+    const { classes } = this.props;
+
+    const contribute_request = JSON.parse(
+      document.getElementById("contribute_request").textContent
+      )['contribute_request'];
+
+    contribute_request.pop();
+    const table = this;
+
+    return (
+      <Paper className={classes.root}>
+      <Table className={classes.table}>
+      <TableHead>
+      <TableRow>
+      <TableCell className={classes.table_title}>User Name</TableCell>
+      <TableCell align="right" className={classes.table_title}>File name</TableCell>
+      <TableCell align="right" className={classes.table_title}>Date Upload</TableCell>
+      <TableCell align="center" className={classes.table_title}>View</TableCell>
+      <TableCell align="center" className={classes.table_title}>Accept</TableCell>
+      </TableRow>
+      </TableHead>
+      <TableBody>
+      {
+        contribute_request.map(function(ct, key){
+          return (
+            <TableRow key={key}>
+            <TableCell component="th" scope="row" className={classes.table_content}>
+            {ct.user_name}
+            </TableCell>
+            <TableCell align="right" className={classes.table_content}>{ct.file_name}</TableCell>
+            <TableCell align="right" className={classes.table_content}>{ct.date_upload}</TableCell>
+            <TableCell align="center" className={classes.table_content}>
+            <Button variant="contained" className={classes.button}>
+            View
+            </Button>
+            </TableCell>
+            <TableCell align="center" className={classes.table_content}>
+            <Button variant="contained" className={classes.button}>
+            Accept
+            </Button>
+            </TableCell>
+            </TableRow>
+            );
+        })
+      }
+
+      </TableBody>
+      </Table>
+      </Paper>
+      );
+  }
+}
+
+RequestTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RequestTable);
