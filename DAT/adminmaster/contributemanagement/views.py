fa9739ad253f911cf2276968e946bb4cdf73546e
Le Dinh Tri <ledinhtri.cse.hcmut@gmail.com>
2019-05-01 01:06:56 +0700

    item contribute view

--- DAT/adminmaster/contributemanagement/views.py
+++ DAT/adminmaster/contributemanagement/views.py
@@ -4,8 +4,10 @@ from rest_framework.response import Response
 from adminmaster.contributemanagement.models import ContributeModel
 from adminmaster.datamanagement.submodels.inputdata import InputDataModel
 import json
+import os
 from django.http import JsonResponse
-
+from adminmaster.datamanagement.submodels.utils.ziprar import ZipRarExtractor
+#from adminmaster.submodels.utils.scanmeta import ScanMetaToDatabase
 
 class ContributeView(generics.RetrieveAPIView):
     lookup_field = 'id'
@@ -37,22 +39,70 @@ class ContributeView(generics.RetrieveAPIView):
                 for file in contribute.input.filter(useful=1)
             ],
         }
-        print(review)
+        #print(review)
         return review
         
-  # @Overwrite
+    # @Overwrite
     def retrieve(self, request, *args, **kwargs):
         __data__ = self.get_queryset()
         return Response(data=__data__)
 
 
-def contribute_view(request, id_file):
-    pass
+class ItemContributeView(generics.RetrieveAPIView):
+    lookup_field = 'id'
+    template_name = 'adminmaster/contrib-items/item.html'
+
+    def lookfiles(self, __folder__, __data__):
+        folders = os.listdir(__folder__)
+        for __file__ in folders:
+            if __file__.split('.')[-1] in ['jpg', 'JPG', 'JPEG', 'jpeg', 'png', 'PNG', 'mp4']:
+                __data__['metas'].append('/gvlab-dat/dataset/' + os.path.join(
+                    __folder__, __file__).replace('\\', '/'))
+            elif (os.path.isdir(os.path.join(__folder__, __file__))):
+                self.lookfiles(os.path.join(__folder__, __file__), __data__)
+
+
+    def get_queryset(self):
+        id_file = self.request.parser_context['kwargs']['id_file']
+        input_file = InputDataModel.objects.filter(id=id_file)
+        
+        ext_file = input_file.first().get_zipname().split('.')[-1]
+        image_file = ext_file in ['jpg', 'JPG', 'JPEG', 'jpeg', 'png', 'PNG']
+        zip_file = ext_file in ['rar', 'zip']
+        media_file = ext_file in ['mp4', 'mp3', ]
+
+        __data__ = {
+            'type': ext_file,
+            'metas': [],
+        }
+
+        if zip_file:
+            __folder__ = input_file.first().get_output_path()
+
+            if not os.path.exists(__folder__):
+                #print("need to extracted file: ", __folder__)
+                zipRarer = ZipRarExtractor(input_file)
+                zipRarer.do_extract_all()
+            else:
+                print("file was extracted")
+                self.lookfiles(__folder__, __data__)
+
+        elif image_file or media_file:
+            __data__['metas'].append(
+                '/gvlab-dat/upload/'+input_file.first().get_full_path_file().replace('\\', '/'))
+        
+        #print(__data__)
+        
+        return __data__
+
+    # @Overwrite
+    def retrieve(self, request, *args, **kwargs):
+        __data__ = self.get_queryset()
+        return Response(data=__data__)
 
 
 def contribute_accept(request, id_file):
     pass
-
 
 def contribute_save(request, id_file):
     pass
