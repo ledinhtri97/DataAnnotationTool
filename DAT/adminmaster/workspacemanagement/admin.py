0495bba7e464e4324e893b12ffb1c4d0982f547b
Le Dinh Tri <ledinhtri.cse.hcmut@gmail.com>
2019-05-08 22:59:21 +0700

    workspace admin add api_reference and change each user have only one settings

--- DAT/adminmaster/workspacemanagement/admin.py
+++ DAT/adminmaster/workspacemanagement/admin.py
@@ -7,95 +7,83 @@ from adminmaster.datamanagement.models import DataSetModel
 # Register your models here.
 
 class WorkSpaceUserForm(forms.ModelForm):
-    class Meta:
-      model = WorkSpaceUserModel
-      fields = '__all__'
+      class Meta:
+            model = WorkSpaceUserModel
+            fields = '__all__'
 
-    def generate_settings(self, dataset, users):
+      def generate_settings(self, users):
+     
+            setts = json.dumps({
+                  'show_popup': 'false',
+                  'auto_hidden': 'false',
+                  'ask_dialog': 'true',
+                  'color_background': 'true',
+                  'size_icon': 8,
+                  'width_stroke': 3,
+            })
+
+            for u in users.all():
+                  if (not UserSettingsModel.objects.filter(user=u).first()):
+                        UserSettingsModel.objects.get_or_create(user=u, settings=setts)
+
+      def save(self, commit=True):
+            instance = super(WorkSpaceUserForm, self).save(commit=False)
+            instance.save()
       
-      sett_wdata = UserSettingsModel.objects.filter(dataset=dataset)
+            self.generate_settings(self.cleaned_data['user'])
 
-      labels = DataSetModel.objects.filter(id=dataset.id).first().labels.all()
+            if(commit):
+                  instance.save()
 
-      colors_map = {
-        str(lb):lb.color for lb in labels
-      }
-
-      setts = json.dumps({
-        'show_popup': 'false',
-        'auto_hidden': 'false',
-        'auto_predict': 'false',
-        'ask_dialog': 'true',
-        'color_background': 'true',
-        'size_icon': 8,
-        'width_stroke': 3,
-      })
-
-      for u in users.all():
-        if (not sett_wdata.filter(user=u).first()):
-          UserSettingsModel.objects.get_or_create(dataset=dataset, user=u, settings=setts)
-
-    def save(self, commit=True):
-      instance = super(WorkSpaceUserForm, self).save(commit=False)
-      instance.save()
-      
-      self.generate_settings(
-        self.cleaned_data['dataset'],
-        self.cleaned_data['user']
-      )
-
-      if(commit):
-         instance.save()
-
-      return instance
+            return instance
 
 class WorkSpaceUserAdmin(admin.ModelAdmin):
-   #class Meta will not accept this form custom > find out why
-   form = WorkSpaceUserForm
+      #class Meta will not accept this form custom > find out why
+      form = WorkSpaceUserForm
 
-   fieldsets = [
-      (None,
-            {
-               'fields': ['nameworkspace']
-            }
-      ),
-      ('DATASET Field',
-            {
-                'fields': ['dataset']
-            }
-      ),
-      ('USERS Field',
-            {
-                'fields': ['user']
-            }
-      ),
-   ]
+      fieldsets = [
+            (None,
+                  {
+                  'fields': ['nameworkspace']
+                  }
+            ),
+            ('DATASET Field',
+                  {
+                  'fields': ['dataset']
+                  }
+            ),
+            ('USERS Field',
+                  {
+                  'fields': ['user']
+                  }
+            ),
+            ('APIS Reference Field',
+                  {
+                  'fields': ['api_reference']
+                  }
+            ),
+      ]
 
 class UserSettingsAdmin(admin.ModelAdmin):
-   #class Meta will not accept this form custom > find out why
+      #class Meta will not accept this form custom > find out why
 
-  readonly_fields = ['settings', 'dataset', 'user']
-   
-  fieldsets = [
-      (None,
-            {
-               'fields': ['settings']
-            }
-      ),
-      ('DATASET Field',
-            {
-                'fields': ['dataset']
-            }
-      ),
-      ('USERS Field',
-            {
-                'fields': ['user']
-            }
-      ),
-   ]
+      readonly_fields = ['settings', 'user']
+            
+      fieldsets = [
+            (None,
+                  {
+                  'fields': ['settings']
+                  }
+            ),
+            ('USERS Field',
+                  {
+                  'fields': ['user']
+                  }
+            ),
+      ]
 
-  def has_delete_permission(self, request, obj=None):
-    return True
+      def has_delete_permission(self, request, obj=None):
+            return True
 
 admin.site.register(WorkSpaceUserModel, WorkSpaceUserAdmin)
 admin.site.register(UserSettingsModel, UserSettingsAdmin)
