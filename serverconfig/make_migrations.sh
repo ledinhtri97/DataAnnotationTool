6312bdd7fe718b4fc40ffb7231014f44f74f90f7
Le Dinh Tri <ledinhtri.cse.hcmut@gmail.com>
2019-05-13 08:02:15 +0700

    make directions mirgations and init file

---
+++ serverconfig/make_migrations.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir DAT/adminmaster/contributemanagement/migrations
+touch DAT/adminmaster/contributemanagement/migrations/__init__.py
+
+mkdir DAT/adminmaster/datamanagement/migrations
+touch DAT/adminmaster/datamanagement/migrations/__init__.py
+
+mkdir DAT/adminmaster/workspacemanagement/migrations
+touch DAT/adminmaster/workspacemanagement/migrations/__init__.py
+
+mkdir DAT/adminmaster/apimodel/migrations
+touch DAT/adminmaster/apimodel/migrations/__init__.py
+
+mkdir DAT/adminmaster/home/migrations
+touch DAT/adminmaster/home/migrations/__init__.py
+
+mkdir DAT/adminmaster/usermaster/migrations
+touch DAT/adminmaster/usermaster/migrations/__init__.py
