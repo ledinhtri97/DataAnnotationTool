9fa94b710757aee0dc5479b34649d6d584df87c5
Le Dinh Tri <ledinhtri.cse.hcmut@gmail.com>
2019-06-28 17:48:43 +0700

    remove old file admin_workspace, global, container and move somefile

--- DAT/templates/__static__/js/admin_workspace.js
+++ DAT/templates/__static__/js/admin_workspace.js
@@ -4,7 +4,4 @@ import ReactDOM from 'react-dom';
 import AdminWorkspace from "./materialui/adminworkspace-ui/adminws-main"
 
 const admin_workspace = document.getElementById("admin_workspace");
-setTimeout(function() {
-	admin_workspace && ReactDOM.render(<AdminWorkspace />, admin_workspace);
-	}, 500
-);
+admin_workspace && ReactDOM.render(<AdminWorkspace />, admin_workspace);
