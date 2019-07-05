7a3430e4a812bb99444995b9a62861a2bd0bc6e8
Tri Le Dinh <triledinh102097@gmail.com>
2019-07-05 12:57:12 +0700

    a

--- DAT/templates/__static__/js/materialui/workspaces-ui.js
+++ DAT/templates/__static__/js/materialui/workspaces-ui.js
@@ -40,7 +40,7 @@ const styles = theme => ({
 		width: 'auto',
 		marginLeft: theme.spacing(3),
 		marginRight: theme.spacing(3),
-		[theme.breakpoints.up(1100 + theme.spacing(6))]: {
+		[theme.breakpoints.up(1100 + theme.spacing(3) * 2)]: {
 			width: 1100,
 			marginLeft: 'auto',
 			marginRight: 'auto',
