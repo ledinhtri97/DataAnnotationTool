972fbf755269baf8584ee425dc009571e7e9df5e
Le Dinh Tri <ledinhtri.cse.hcmut@gmail.com>
2019-07-06 08:57:45 +0700

    remove comment

--- DAT/usermaster/subviews/request/querymeta.py
+++ DAT/usermaster/subviews/request/querymeta.py
@@ -51,7 +51,7 @@ def get_fake_api(meta, api_ref):
             print(e)
             data = [{'error': 'Failed to connect'}]
         #{'data':{'boxes':[{'conf', 'label', 'xmax', 'ymax', 'xmin', 'ymin'}], '...parameters'}
-    print(data)
+    #print(data)
     return data
 
 
