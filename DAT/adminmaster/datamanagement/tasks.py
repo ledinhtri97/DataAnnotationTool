8b44fec974d18f4a45855919f90e0e8cf576205b
Tri Le Dinh <triledinh102097@gmail.com>
2019-07-30 18:07:58 +0700

    coding tracking feature...

--- DAT/adminmaster/datamanagement/tasks.py
+++ DAT/adminmaster/datamanagement/tasks.py
@@ -137,6 +137,25 @@ def extract_seqframevideo(datasetid):
             #     settings.STORAGE_DIR, full_path_folder))
 
             # lookfiles(input_data.get_output_path())
+
+            """ import cv2
+                vidcap = cv2.VideoCapture('video.mp4')
+                def getFrame(sec):
+                    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+                    hasFrames,image = vidcap.read()
+                    if hasFrames:
+                        cv2.imwrite("image"+str(count)+".jpg", image)     # save frame as JPG file
+                    return hasFrames
+                sec = 0
+                frameRate = 0.5 #//it will capture image in each 0.5 second
+                count=1
+                success = getFrame(sec)
+                while success:
+                    count = count + 1
+                    sec = sec + frameRate
+                    sec = round(sec, 2)
+                    success = getFrame(sec)
+            """
             
             if (input_data.groundtruth):
                 INPUT_FILE = os.path.join(
