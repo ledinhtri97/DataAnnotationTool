5b4aab0f26cc40c0bfb6660d14738ebbf79e8d95
Tri Le Dinh <triledinh102097@gmail.com>
2019-10-11 14:11:27 +0700

    fix: add is_annotated checking

--- DAT/usermaster/subviews/request/labeling_view.py
+++ DAT/usermaster/subviews/request/labeling_view.py
@@ -173,9 +173,13 @@ def savenext_index(request, metaid):
 
 			if bottom_meta is not None:
 				top_meta.next_link = bottom_meta
+				top_meta.is_annotated = True
 			
 			if top_meta is not None:
 				bottom_meta.pre_link = top_meta
+			
+			if bottom_meta.next_link:
+				bottom_meta.is_annotated = True
 
 			top_meta.submitted_by_user.add(user)
 			top_meta.onviewing_user = None
